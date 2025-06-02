b81e3f822f66bed3f6098d35dff11e9c86b24296
JBA <JBA@LAPTOP-RVGG3I2B>
2025-06-02 11:27:56 -0600

    Diseño forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(376, 116);
+            this.button1.Location = new System.Drawing.Point(376, 123);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 64);
             this.button1.TabIndex = 0;
@@ -174,7 +174,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "S";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
